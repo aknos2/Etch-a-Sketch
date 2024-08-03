e2951245c0a1d9e54bfd66078656e16bd713f357
Aknos <161412826+eltontomiyoshi@users.noreply.github.com>
2024-08-03 18:15:16 +0900

    add flexBasis cal method

--- javascript.js
+++ javascript.js
@@ -4,36 +4,51 @@ const container = document.querySelector("#container");
 const selectNumber = 100;
 
 function button() {
+    //create select button
     let button = document.createElement("select");
     button.id = "button";
     document.body.insertBefore(button, container);
 
-    for( let y = 0; y <= selectNumber; y++) {
+    //create and append options to select element
+    for( let v = 1; v <= selectNumber; v++) {
         let option = document.createElement("option");
-        option.value = y;
-        option.text = y;
+        option.value = v;
+        option.text = v;
+        if (v === 16) {
+            option.selected = true;
+        };
         button.appendChild(option);
     };
 
     button.addEventListener("change", event => {
-    
+        container.innerHTML = "";
+        createGrid((event.target.value) * (event.target.value));
+
+        const squares = document.querySelectorAll(".square");
+        squares.forEach(square => {
+             square.style.flexBasis = `calc(100% / ${event.target.value})`
+        })
     });
 }
 
 function createSquare() {
-    let square = document.createElement("div");
+    const square = document.createElement("div");
     container.appendChild(square);
     square.classList.add("square");
 
-    let content = document.createElement("div");
+    const content = document.createElement("div");
     square.appendChild(content);
     content.classList.add("content");
 }
 
-function createSixteenGrid() {
-    for (let i = 1; i < (16 * 16 + 1); i++) {
+function createGrid(size) {
+    for(let i = 1; i <= size; i++) {
         createSquare(i);
     }
+}
+
+function createSixteenGrid() {
+    createGrid(16 * 16);
 }
 
 button()
